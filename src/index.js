e677767aa03f6de6336d9186b04ce50abb6d7ea4
Thomas Russell <t.p.russell19@gmail.com>
2021-07-12 20:26:24 -0700

    add files and code through lesson 7

--- src/index.js
+++ src/index.js
@@ -6,17 +6,29 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import rootReducer from './reducers/index';
 import { Provider } from 'react-redux';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
 
 const store = createStore(rootReducer);
 
-
+const rrfProps = {
+  firebase,
+  config: {
+        userProfile: "users"
+    },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
 import formVisibleReducer from './form-visible-reducer';
 import ticketListReducer from './ticket-list-reducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  masterTicketList: ticketListReducer
+  masterTicketList: ticketListReducer,
+  // new line of code below
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
